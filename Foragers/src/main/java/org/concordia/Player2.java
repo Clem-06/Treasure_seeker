638ff448a267b8bb66fd302aefe85354a8c3f1df
Clement Lepage <cl_lepag@live.concordia.ca>
2026-03-31 15:45:23 -0400

    fixedp2

--- Foragers/src/main/java/org/concordia/Player2.java
+++ Foragers/src/main/java/org/concordia/Player2.java
@@ -49,47 +49,47 @@ public class Player2 {
         return best;
     }
 
-//    public Tile moveDecision(GameState state) {
-//        Tile current = state.tiles[y][x];
-//
-////        System.out.println("Current P2 manahtan to nearest treasure:  " + manhattanToNearestTreasure(state));
-//
-//
-//        int step = state.rounds_left;
-//        int phase = 3 - ((step / 15) % 4); // ensures N → W → S → E order
-//
-//        int r = ThreadLocalRandom.current().nextInt(0, 3);
-//
-//        Tile next = null;
-//
-//        switch (phase) {
-//            case 0: // N
-//                if (r == 0) next = current.getN();
-//                else if (r == 1) next = current.getNW();
-//                else next = current.getNE();
-//                break;
-//
-//            case 1: // W
-//                if (r == 0) next = current.getW();
-//                else if (r == 1) next = current.getNW();
-//                else next = current.getSW();
-//                break;
-//
-//            case 2: // S
-//                if (r == 0) next = current.getS();
-//                else if (r == 1) next = current.getSW();
-//                else next = current.getSE();
-//                break;
-//
-//            case 3: // E
-//                if (r == 0) next = current.getE();
-//                else if (r == 1) next = current.getNE();
-//                else next = current.getSE();
-//                break;
-//        }
-//
-//        return next;
-//    }
+    public Tile moveDecision(GameState state) {
+        Tile current = state.tiles[y][x];
+
+//        System.out.println("Current P2 manahtan to nearest treasure:  " + manhattanToNearestTreasure(state));
+
+
+        int step = state.rounds_left;
+        int phase = 3 - ((step / 15) % 4); // ensures N → W → S → E order
+
+        int r = ThreadLocalRandom.current().nextInt(0, 3);
+
+        Tile next = null;
+
+        switch (phase) {
+            case 0: // N
+                if (r == 0) next = current.getN();
+                else if (r == 1) next = current.getNW();
+                else next = current.getNE();
+                break;
+
+            case 1: // W
+                if (r == 0) next = current.getW();
+                else if (r == 1) next = current.getNW();
+                else next = current.getSW();
+                break;
+
+            case 2: // S
+                if (r == 0) next = current.getS();
+                else if (r == 1) next = current.getSW();
+                else next = current.getSE();
+                break;
+
+            case 3: // E
+                if (r == 0) next = current.getE();
+                else if (r == 1) next = current.getNE();
+                else next = current.getSE();
+                break;
+        }
+
+        return next;
+    }
 
     public void updatePlayer(GameState state) {
         this.x = state.p2_x;
