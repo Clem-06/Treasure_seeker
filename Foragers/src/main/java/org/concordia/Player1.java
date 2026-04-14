46c21d8f9b63fc1f9c06eba65cc1e358c96caf73
Clement Lepage <cl_lepag@live.concordia.ca>
2026-04-13 23:43:35 -0400

    added treasure remove for closest to p2 so p1 doesnt chasee after

--- Foragers/src/main/java/org/concordia/Player1.java
+++ Foragers/src/main/java/org/concordia/Player1.java
@@ -1,5 +1,6 @@
 package org.concordia;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Player1 {
@@ -8,8 +9,7 @@ public class Player1 {
 	public int x;
 	public int y;
 	public int score;
-	int calls;
-	int totalCellsChecked;
+
 
 	private int totalPositionsEvaluated;
 	public int totalPruned = 0;
@@ -26,10 +26,8 @@ public class Player1 {
 		int treasureTargetX = s.p1_x;
 		int treasureTargetY = s.p1_y;
 
-		calls++;
 
 		for (int d = 0; d <= maxDistance; d++) {
-			totalCellsChecked ++;
 
 
 			// Top and bottom rows
@@ -40,11 +38,11 @@ public class Player1 {
 				int nyBottom = treasureTargetY + d;
 
 				if (inBounds(nx, nyTop)) {
-					if (s.tiles[nyTop][nx].treasurePresent && treasures[XYtoI(nx, nyTop)]) return d;
+					if (treasures[XYtoI(nx, nyTop)]) return d;
 				}
 
 				if (inBounds(nx, nyBottom)) {
-					if (s.tiles[nyBottom][nx].treasurePresent && treasures[XYtoI(nx, nyBottom)]) return d;
+					if (treasures[XYtoI(nx, nyBottom)]) return d;
 				}
 			}
 
@@ -56,11 +54,11 @@ public class Player1 {
 				int nxRight = treasureTargetX + d;
 
 				if (inBounds(nxLeft, ny)) {
-					if (s.tiles[ny][nxLeft].treasurePresent && treasures[XYtoI(nxLeft, ny)]) return d;
+					if (treasures[XYtoI(nxLeft, ny)]) return d;
 				}
 
 				if (inBounds(nxRight, ny)) {
-					if (s.tiles[ny][nxRight].treasurePresent && treasures[XYtoI(nxRight, ny)]) return d;
+					if (treasures[XYtoI(nxRight, ny)]) return d;
 				}
 			}
 		}
@@ -122,7 +120,7 @@ public class Player1 {
 		} else {//diagonals and treasures if not crowded
 			for (int i = 0; i < 8; i++) {
 				if (current.neighbours[i] != null) {
-					if (current.neighbours[i].treasurePresent || isDiagonal(i)) {
+					if (current.neighbours[i].treasurePresent || isDiagonal(i)) {//FIX MOVE ORDEIRNG SO TREASURES ARE PUT IN PRIORITY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 						tilesOfInterest.add(current.neighbours[i]);
 					}
 				}
@@ -154,7 +152,7 @@ public class Player1 {
 			float neighborSearchEval = search(simulation, treasures, depth - 1, null, false);
 			//no need to pass bestTileHodler down children aren't root
 
-			if(isRoot){
+			if (isRoot) {
 				neighborSearchEval += 0.1f * eval(simulation, treasures);//maybe works as tiebreaker so picks treasure up early
 			}
 
@@ -164,11 +162,9 @@ public class Player1 {
 			}
 
 			if (depth == 12) {
-				System.out.println("Neighbor's search eval (best down that path):  " + neighborSearchEval);
-				float neighborTrueEval = eval(simulation, treasures);
-				System.out.println("Neighbor's eval (evaluate position after 1 move made):  " + neighborTrueEval);
-				System.out.println("Average depth of treasure " +
-						totalCellsChecked / calls);
+//				System.out.println("Neighbor's search eval (best down that path):  " + neighborSearchEval);
+//				float neighborTrueEval = eval(simulation, treasures);
+//				System.out.println("Neighbor's eval (evaluate position after 1 move made):  " + neighborTrueEval);
 			}
 
 			if (neighborSearchEval > bestEval) {
@@ -186,13 +182,41 @@ public class Player1 {
 
 		boolean[] treasures = new boolean[MapLoader.MAP_HEIGHT * MapLoader.MAP_WIDTH];
 
-		for (int y = 0; y < MapLoader.MAP_HEIGHT; y++) {
+		for (int y = 0; y < MapLoader.MAP_HEIGHT; y++) {//create flattened 2d boolean array of treasures for fast retreival
 			for (int x = 0; x < MapLoader.MAP_WIDTH; x++) {
-				if (originalState.tiles[y][x].treasurePresent) {//FIX MOVE ORDEIRNG SO TREASURES ARE PUT IN PRIORITY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				if (originalState.tiles[y][x].treasurePresent) {
 					treasures[XYtoI(x, y)] = true;
 				}
 			}
 		}
+
+		//need to remove the treasure closest to p2 if its farther from p1
+		int bestDistanceP2 = 999;
+		int newDistanceP2;
+
+		int bestP2X = 0;
+		int bestP2Y = 0;
+
+		for (int mapY = 0; mapY < MapLoader.MAP_HEIGHT; mapY++) {
+			for (int mapX = 0; mapX < MapLoader.MAP_WIDTH; mapX++) {
+				if (originalState.tiles[mapY][mapX].treasurePresent) {
+					newDistanceP2 = Math.max(Math.abs(originalState.p2_x - mapX), Math.abs(originalState.p2_y - mapY)); // Maxhattan for P2
+
+					if(newDistanceP2 < bestDistanceP2) {
+						bestDistanceP2 = newDistanceP2;
+						bestP2X = mapX;
+						bestP2Y = mapY;
+					}
+				}
+			}
+		}
+		int distanceP1 = Math.max(Math.abs(originalState.p1_x - bestP2X ), Math.abs(originalState.p1_y - bestP2Y));
+		if(bestDistanceP2 <= distanceP1){//remove treasure at bestX bestY if the nearest treasure to P2 is also further from P1 or equal
+			treasures[XYtoI(bestP2X, bestP2Y)] = false; //pretend there is no treasure as we dont want p1 chasing something p2 will get first
+			System.out.println("treasure removed at: " +  bestP2X + ", " + bestP2Y);
+		}
+
+
 
 		Tile[] bestTileHolder = new Tile[1]; //create array we pass by reference in search to get best tile
 
@@ -203,7 +227,6 @@ public class Player1 {
 		System.out.println("Total positions searched: ");
 		System.out.printf("%.1E%n", (double) totalPositionsEvaluated);
 		System.out.println("Rounds left: " + originalState.rounds_left);
-		System.out.printf("");
 
 
 		return bestTileHolder[0];
